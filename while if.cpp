91f4c4aa4955d457269ce90410f48d07cb1f03d7
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-09-26 19:20:46 +0800

    Add files via upload
    code=nisaa

---
+++ while if.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	string code="nisaa";
+	string match;
+	
+	
+	while (true) {
+
+        cout << "Enter your code= ";
+        cin >> match;
+	
+		if(code==match){
+			cout<<"I love you"<<endl;
+			cout<<"    <3 <3 <3 <3 <3       <3 <3 <3 <3 <3 "<<endl;
+			cout<<" <3 <3 <3 <3 <3 <3 <3<3 <3 <3 <3<3 <3 <3"<<endl;
+			cout<<"<3 <3 <3 <3 <3 <3 <3<3 <3 <3 <3<3 <3 <3 <3"<<endl;
+			cout<<"<3 <3 <3 <3 <3 <3 <3 <3<3 <3 <3 <3<3 <3 <3"<<endl;
+			cout<<"<3 <3 <3 <3 <3 <3 <3 <3<3 <3 <3 <3<3 <3 <3 "<<endl;
+			cout<<"   <3 <3 <3 <3 <3 <3 <3<3 <3 <3 <3<3 <3 <3   "<<endl;
+			cout<<"     <3 <3 <3 <3 <3<3 <3 <3 <3<3 <3 <3  "<<endl;
+			cout<<"         <3 <3 <3 <3 <3<3 <3 <3 <3  "<<endl;
+			cout<<"           <3 <3 <3 <3<3 <3 <3  "<<endl;
+			cout<<"            <3 <3 <3 <3 <3  "<<endl;
+			cout<<"                 <3 <3 "<<endl;
+			cout<<"                  <3  "<<endl;
+			
+			break;
+		}
+		else{
+		
+		cout<<"We are not meant to be"<<endl;
+		break;}
+		};
+		
+			}
+	
+	
+
